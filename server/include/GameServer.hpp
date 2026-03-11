20c05ffe71d36d30ea7741b1e5e6a626f35b9923
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-03-11 18:30:16 +0300

    Create GameServer.hpp

---
+++ server/include/GameServer.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "Types.hpp"
+#include "PlayerCommand.hpp"
+#include "ThreadSafeQueue.hpp"
+#include "ThreadPool.hpp"
+#include "World.hpp"
+#include "ResourceManager.hpp"
+#include "TickScheduler.hpp"
+#include "NetworkManager.hpp"
+
+#include <memory>
+#include <atomic>
+#include <vector>
+
+namespace multilife
+{
+
+    class GameServer {
+    public:
+        GameServer(std::unique_ptr<NetworkManager> networkManager,
+                   std::size_t workerThreads,
+                   std::chrono::milliseconds tickInterval);
+
+        ~GameServer();
+
+        GameServer(const GameServer&) = delete;
+        GameServer& operator=(const GameServer&) = delete;
+
+        void start(std::uint16_t port);
+
+        void stop();
+
+        [[nodiscard]] bool isRunning() const noexcept {
+            return m_running.load(std::memory_order_relaxed);
+        }
+
+        World& world() noexcept {
+            return m_world;
+        }
+        ResourceManager& resources() noexcept {
+            return m_resourceManager;
+        }
+
+    private:
+        void onCommandsFromNetwork(std::vector<PlayerCommand> commands);
+        void onTick();
+
+        std::unique_ptr<NetworkManager> m_networkManager;
+        ThreadPool                      m_threadPool;
+        World                           m_world;
+        ResourceManager                 m_resourceManager;
+        TickScheduler                   m_tickScheduler;
+
+        ThreadSafeQueue<PlayerCommand>  m_commandQueue;
+        std::atomic<bool>               m_running{false};
+    };
+
+} // namespace multilife
+
