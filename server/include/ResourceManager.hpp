f5a97bbbf84f690364d54b75d99fa5e03eb970d0
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-03-11 17:09:10 +0300

    Create ResourceManager.hpp

---
+++ server/include/ResourceManager.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Types.hpp"
+
+#include <unordered_map>
+#include <shared_mutex>
+#include <mutex>
+
+namespace multilife
+{
+
+    class ResourceManager
+    {
+    public:
+        ResourceManager() = default;
+
+        std::uint64_t getBalance(PlayerId playerId) const;
+
+        bool trySpend(PlayerId playerId, std::uint64_t amount);
+
+        void awardFromLiveCounts(const std::unordered_map<PlayerId, std::uint64_t>& liveCounts);
+
+    private:
+        mutable std::shared_mutex m_mutex;
+        std::unordered_map<PlayerId, std::uint64_t> m_balances;
+    };
+
+} // namespace multilife
+
