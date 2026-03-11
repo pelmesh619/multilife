6c85401c99e54b9d73457d80135e14aa0ee3c240
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-03-11 16:58:12 +0300

    Create Chunk.hpp

---
+++ server/include/Chunk.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "Types.hpp"
+
+#include <array>
+#include <unordered_map>
+
+namespace multilife
+{
+
+    class Chunk {
+    public:
+        static constexpr std::size_t InnerWidth  = ChunkWidth;
+        static constexpr std::size_t InnerHeight = ChunkHeight;
+        static constexpr std::size_t GhostBorder = 1;
+        static constexpr std::size_t TotalWidth  = InnerWidth + 2 * GhostBorder;
+        static constexpr std::size_t TotalHeight = InnerHeight + 2 * GhostBorder;
+        static constexpr std::size_t TotalCells  = TotalWidth * TotalHeight;
+
+        Chunk() = default;
+
+        CellState getCell(std::size_t x, std::size_t y) const noexcept;
+        void setCell(std::size_t x, std::size_t y, const CellState& state) noexcept;
+
+        CellState getGhostCell(std::size_t storageX, std::size_t storageY) const noexcept;
+        void setGhostCell(std::size_t storageX, std::size_t storageY, const CellState& state) noexcept;
+
+        void calculateNext();
+
+        void swapBuffers() noexcept;
+
+        std::unordered_map<PlayerId, std::uint64_t> getLiveCountByPlayer() const;
+
+        void clear();
+
+    private:
+        using Buffer = std::array<CellState, TotalCells>;
+
+        static std::size_t index(std::size_t storageX, std::size_t storageY) noexcept
+        {
+            return toIndex(storageX, storageY, TotalWidth);
+        }
+
+        std::uint8_t m_currentBufferIndex{0};
+        Buffer m_buffers[2]{};
+    };
+
+} // namespace multilife
+
