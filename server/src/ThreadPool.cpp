a1aed33408cdded8a7ab5e708c0259b97213f94d
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-03-11 18:39:23 +0300

    Create ThreadPool.cpp

---
+++ server/src/ThreadPool.cpp
@@ -0,0 +1,74 @@
+#include "ThreadPool.hpp"
+
+namespace multilife
+{
+
+    ThreadPool::ThreadPool(std::size_t threadCount) {
+        if (threadCount == 0) {
+            threadCount = 1;
+        }
+
+        m_workers.reserve(threadCount);
+        for (std::size_t i = 0; i < threadCount; ++i) {
+            m_workers.emplace_back([this]() { workerLoop(); });
+        }
+    }
+
+    ThreadPool::~ThreadPool() {
+        shutdown();
+    }
+
+    void ThreadPool::enqueue(std::function<void()> task) {
+        {
+            std::lock_guard<std::mutex> lock(m_mutex);
+            if (m_stopping.load(std::memory_order_relaxed)) {
+                return;
+            }
+            m_tasks.push(std::move(task));
+        }
+        m_cv.notify_one();
+    }
+
+    void ThreadPool::shutdown() {
+        bool expected = false;
+        if (!m_stopping.compare_exchange_strong(expected, true)) {
+            return;
+        }
+
+        {
+            std::lock_guard<std::mutex> lock(m_mutex);
+        }
+        m_cv.notify_all();
+
+        for (auto& worker : m_workers) {
+            if (worker.joinable()) {
+                worker.join();
+            }
+        }
+    }
+
+    void ThreadPool::workerLoop() {
+        for (;;) {
+            std::function<void()> task;
+            {
+                std::unique_lock<std::mutex> lock(m_mutex);
+                m_cv.wait(lock, [this]() {
+                    return m_stopping.load(std::memory_order_relaxed) || !m_tasks.empty();
+                });
+
+                if (m_stopping.load(std::memory_order_relaxed) && m_tasks.empty()) {
+                    return;
+                }
+
+                task = std::move(m_tasks.front());
+                m_tasks.pop();
+            }
+
+            if (task) {
+                task();
+            }
+        }
+    }
+
+} // namespace multilife
+
