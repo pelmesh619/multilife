d88fb4a034586ca231a50f24d32252c40d81cc8a
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-04-16 14:33:18 +0300

    Update ResourceManager.hpp

--- server/include/ResourceManager.hpp
+++ server/include/ResourceManager.hpp
@@ -14,11 +14,17 @@ namespace multilife
     public:
         ResourceManager() = default;
 
+        std::uint64_t addPlayer(PlayerId playerId);
+
         std::uint64_t getBalance(PlayerId playerId) const;
 
         bool trySpend(PlayerId playerId, std::uint64_t amount);
 
+        bool award(PlayerId playerId, std::uint64_t amount);
+
         void awardFromLiveCounts(const std::unordered_map<PlayerId, std::uint64_t>& liveCounts);
+
+        std::vector<PlayerId> getPlayerIds() const;
 
     private:
         mutable std::shared_mutex m_mutex;
