4d66869e8764290051515ae20ea88cd7bbffaeb8
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-08-24 13:47:58 +0300

    Create main.cpp

---
+++ client/src/main.cpp
@@ -0,0 +1,11 @@
+#include <QApplication>
+
+#include "GameWindow.h"
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+    GameWindow window;
+    window.show();
+    return app.exec();
+}
